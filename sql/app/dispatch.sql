bcaba9189b3c52c9fcaefc7d253ead438d9be04a
nicola <niquola@gmail.com>
2014-02-17 16:10:51 +0400

    simplify

--- sql/app/dispatch.sql
+++ sql/app/dispatch.sql
@@ -1,5 +1,29 @@
 --db:myfhir
 --{{{
+CREATE SCHEMA IF NOT EXISTS app;
+
+DROP TABLE IF EXISTS app.routes;
+
+CREATE TABLE app.routes (
+  id serial primary key,
+  method varchar,
+  pattern varchar,
+  handler varchar
+);
+
+INSERT INTO app.routes
+(method, pattern, handler)
+VALUES
+('GET', '/data/about', 'about'),
+('GET', '/data/view', 'view'),
+('GET', '/data/query', 'query'),
+('GET', '/data/resource', 'resource'),
+('GET', '/data/demo', 'demo'),
+('GET', '/data/demo/by_attr', 'demo_by_attr'),
+('POST', '/data/resource/create', 'create_resource')
+;
+--}}}
+--{{{
 CREATE SCHEMA IF NOT EXISTS lib;
 CREATE EXTENSION IF NOT EXISTS plv8;
 
