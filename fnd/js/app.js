59be9f669af987434571c0666a2e5fcc7e56b9f9
Serzh Nechyporchuk <nechyporchuk@gmail.com>
2014-02-26 11:05:38 +0200

    add compact filter

--- fnd/js/app.js
+++ fnd/js/app.js
@@ -329,3 +329,24 @@ app.controller('ResourceSelectCtrl', function($scope, $filter, Query) {
       window.wizard.patientId + "'";
   }
 })
+
+app.filter('compact', function() {
+  return function compactFilter(obj) {
+    for (var key in obj) {
+      if (!obj[key]) {
+        if (angular.isObject(obj)) {
+          delete obj[key];
+        } else if (angular.isArray(obj)) {
+          obj.splice(key, 1);
+        }
+      } else if (angular.isObject(obj[key])) {
+        compactFilter(obj[key]);
+      } else if (angular.isArray(obj[key])) {
+        for (var subobj in obj[key]) {
+          compactFilter(subobj);
+        }
+      }
+    }
+    return obj;
+  }
+})
