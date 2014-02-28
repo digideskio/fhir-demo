ae3f9fbe377acb42ada5534693973fbaf975b98f
Sasha Alexandrov <elequtree@gmail.com>
2014-02-28 16:30:58 +0400

    Save

--- sql/app/actions.sql
+++ sql/app/actions.sql
@@ -9,7 +9,7 @@ FUNCTION actions.about(req json)
 RETURNS json
 language plv8
 as $$
- return {message: 'here is about page', request: req}
+  return {message: 'here is about page', request: req}
 $$;
 --}}}
 
@@ -29,7 +29,7 @@ FUNCTION actions.get_tables(req json)
 RETURNS json
 language plv8
 as $$
- return plv8.execute("SELECT array_to_json(array_agg(t.*)) as json from test.expanded_resource_tables t")[0]['json']
+  return plv8.execute("SELECT array_to_json(array_agg(t.*)) as json from test.expanded_resource_tables t")[0]['json']
 $$;
 
 --{{{
@@ -38,7 +38,7 @@ FUNCTION actions.show(req json)
 RETURNS json
 language plv8
 as $$
- return plv8.execute("SELECT array_to_json(array_agg(row_to_json(t.*)))::varchar as json from test.expanded_resource_tables t where resource_name = '" + req.uri_args.resource + "'")[0]['json']
+  return plv8.execute("SELECT array_to_json(array_agg(row_to_json(t.*)))::varchar as json from test.expanded_resource_tables t where resource_name = '" + req.uri_args.resource + "'")[0]['json']
 $$;
 
 --SELECT actions.show('{"uri_args": {"resource": "Alert"}}'::json);
@@ -50,12 +50,13 @@ FUNCTION actions.delete_resource(req json)
 RETURNS json
 language plv8
 as $$
- var resource_id = req.uri_args.resource_id;
- var sql = "select fhir.delete_resource('" + resource_id + "'::uuid)";
- plv8.elog(NOTICE, sql);
- plv8.execute(sql);
- 
- return JSON.stringify({id: resource_id });
+  var resource_id = req.uri_args.resource_id;
+  var sql = "select fhir.delete_resource('" + resource_id + "'::uuid)";
+
+  plv8.elog(NOTICE, sql);
+  plv8.execute(sql);
+
+  return JSON.stringify({id: resource_id });
 $$;
 
 --SELECT actions.delete_resource(('{"uri_args": {"resource_id": "' || (select id from fhir.resource limit 1)::varchar || '"}}')::json);
@@ -90,52 +91,56 @@ as $$
   var resource_tables = plv8.execute("SELECT t.* from test.expanded_resource_tables t where resource_name = '" + resource_type + "'");
   var res = [];
   for(var i=0; i<resource_tables.length; i++){
-				var rt = resource_tables[i];
-				var table_name = rt['table_name'];
-				var resource_name = rt['resource_name'];
-				var path = rt['path'];
-				if (table_name != 'patient_photo') {
-				var columns = resource_tables[i]['columns'];
-				var where = 'id'
-				for (var j=0; j< columns.length; j++) {
-							if (columns[j]['column_name'] == 'resource_id') {
-												where = 'resource_id';
-												}
-								}
-				var sel = plv8.execute("SELECT * from fhir." + table_name + " where " + where + " = '" + resource_id + "'");
-        for(var m=0;m<sel.length;m++){
-            var row = sel[m];
-            for (var jj=0; jj< columns.length; jj++) {
-              var col_name = columns[jj]['column_name'];
-              if (row[col_name]) {
-                var data_type = columns[jj]['data_type'];
-                if (data_type == 'uuid') {
-                  row[col_name] = row[col_name].split('-')[0] + '...';
-                } else if (data_type == 'date' || data_type == 'timestamp without time zone') {
-                
-                  row[col_name] = new Date(Date.parse(row[col_name])).toString();
-                }
+    var rt = resource_tables[i];
+    var table_name = rt['table_name'];
+    var resource_name = rt['resource_name'];
+    var columns = resource_tables[i]['columns'];
+    var path = rt['path'];
+
+    if (table_name == 'patient_photo') {
+      continue;
+    }
+
+    var where = 'id'
+    for (var j=0; j< columns.length; j++) {
+      if (columns[j]['column_name'] == 'resource_id') {
+        where = 'resource_id';
+      }
+    }
+
+    var sel = plv8.execute("SELECT * from fhir." + table_name + " where " + where + " = '" + resource_id + "'");
+    for(var m=0;m<sel.length;m++){
+        var row = sel[m];
+        for (var jj=0; jj< columns.length; jj++) {
+          var col_name = columns[jj]['column_name'];
+          if (row[col_name]) {
+            var data_type = columns[jj]['data_type'];
+            if (data_type == 'uuid') {
+              row[col_name] = row[col_name].split('-')[0] + '...';
+            } else if (data_type == 'date' || data_type == 'timestamp without time zone') {
+              row[col_name] = new Date(Date.parse(row[col_name])).toString();
             }
-          }
         }
-				if (sel.length > 0) {
-								var headers = [];
-								for(var k in sel[0]) {
-												var is = false;
-												for(var m=0;m<sel.length;m++){
-																if (sel[m][k]) {
-																is = true;
-																}
-																}
-												if (is) {headers.push(k);}
-												}
-								var obj = {'table_name': table_name, 'path': path, headers: headers, data: sel};
-								res.push(obj);
-				}
-				}
-		}
-   var json = {'resource_name': resource_name, 'resource_id': resource_id, data: res}
-	 return JSON.stringify(json);
+      }
+    }
+    if (sel.length > 0) {
+            var headers = [];
+            for(var k in sel[0]) {
+                    var is = false;
+                    for(var m=0;m<sel.length;m++){
+                            if (sel[m][k]) {
+                            is = true;
+                            }
+                            }
+                    if (is) {headers.push(k);}
+                    }
+            var obj = {'table_name': table_name, 'path': path, headers: headers, data: sel};
+            res.push(obj);
+    }
+  }
+
+  var json = {'resource_name': resource_name, 'resource_id': resource_id, data: res}
+  return JSON.stringify(json);
 $$;
 
 --SELECT actions.details(('{"uri_args": {"resource_id": "' || (select id from fhir.resource limit 1)::varchar || '"}}')::json);
@@ -174,12 +179,12 @@ as $$
   var res = [];
   var list = plv8.execute('select id, _type from fhir.resource');
   for(var i=0; i<list.length; i++){
-				var id = list[i]['id'];
-				var view = list[i]['_type'];
-				var obj = plv8.execute( "SELECT t.json from fhir.view_" + view + ' t')[0]['json'];
-				res.push(obj);
-				}
-	 return JSON.stringify(res);
+        var id = list[i]['id'];
+        var view = list[i]['_type'];
+        var obj = plv8.execute( "SELECT t.json from fhir.view_" + view + ' t')[0]['json'];
+        res.push(obj);
+        }
+   return JSON.stringify(res);
  var sql =  "SELECT array_to_json(array_agg(t.json))::varchar as json from fhir.view_" + req.uri_args.type + ' t limit 100'
  plv8.elog(NOTICE, sql)
  return plv8.execute(sql)[0]['json'];
