1f84a2348f4ced81744f08e80e280eae710f0807
Maksym Bodnarchuk <bodnarchuk@gmail.com>
2014-03-17 11:27:35 +0200

    fix

--- sbin/frontend.sh
+++ sbin/frontend.sh
@@ -1,6 +1,6 @@
 function install_front {
   cd fnd
-  which nvm || (echo 'install nvm' && exit 1)
+  which node || (echo 'install nvm && node' && exit 1)
   nvm use 0.10
   npm install -g bower
   bower install
