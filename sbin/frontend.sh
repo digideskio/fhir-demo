495d4a3cda6b855b022931ed2692585c2ed8ed89
Maksym Bodnarchuk <bodnarchuk@gmail.com>
2014-03-17 11:59:05 +0200

    fix

--- sbin/frontend.sh
+++ sbin/frontend.sh
@@ -1,6 +1,15 @@
 function install_front {
+  pwd
   cd fnd
+  pwd
+  ls
+  id
+  source ~/.nvm/nvm.sh
+  nvm --help
+  nvm list
   nvm use 0.10
+  nvm list
   npm install -g bower
   bower install
+  echo zashibis
 }
